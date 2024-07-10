9e953c26b240e0f32b99b356e7ac1a16a9ef4d43
zionweb <92651245+zionweb@users.noreply.github.com>
2024-07-10 20:08:34 -0300

    se crearon 12 task definitions y modificaron services

--- iac/task-definitions.tf
+++ iac/task-definitions.tf
@@ -1,6 +1,7 @@
-resource "aws_ecs_task_definition" "products_definition" {
+# TASK DEFINITION PRODUCTS #
 
-  family                   = "products-task-definition"
+resource "aws_ecs_task_definition" "dev_products_definition" {
+  family                   = "dev-products-task-definition"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
@@ -24,7 +25,7 @@ resource "aws_ecs_task_definition" "products_definition" {
     logConfiguration = {
       logDriver = "awslogs"
       options = {
-        awslogs-group         = "/ecs/products-task-definition"
+        awslogs-group         = "/ecs/dev-products-task-definition"
         awslogs-create-group  = "true"
         awslogs-region        = "us-east-1"
         awslogs-stream-prefix = "ecs"
@@ -36,13 +37,12 @@ resource "aws_ecs_task_definition" "products_definition" {
     operating_system_family = "LINUX"
   }
   tags = {
-    Name = "products-task-definition"
+    Name = "dev-products-task-definition"
   }
 }
 
-resource "aws_ecs_task_definition" "shipping_definition" {
-
-  family                   = "shipping-task-definition"
+resource "aws_ecs_task_definition" "test_products_definition" {
+  family                   = "test-products-task-definition"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
@@ -51,8 +51,8 @@ resource "aws_ecs_task_definition" "shipping_definition" {
   execution_role_arn       = "arn:aws:iam::931481537897:role/LabRole"
 
   container_definitions = jsonencode([{
-    name      = "shipping-container"
-    image     = "931481537897.dkr.ecr.us-east-1.amazonaws.com/shipping-service-ecr-repo:latest"
+    name      = "products-container"
+    image     = "931481537897.dkr.ecr.us-east-1.amazonaws.com/products-service-ecr-repo:latest"
     cpu       = 0
     essential = true
 
@@ -66,7 +66,7 @@ resource "aws_ecs_task_definition" "shipping_definition" {
     logConfiguration = {
       logDriver = "awslogs"
       options = {
-        awslogs-group         = "/ecs/shipping-task-definition"
+        awslogs-group         = "/ecs/test-products-task-definition"
         awslogs-create-group  = "true"
         awslogs-region        = "us-east-1"
         awslogs-stream-prefix = "ecs"
@@ -78,23 +78,22 @@ resource "aws_ecs_task_definition" "shipping_definition" {
     operating_system_family = "LINUX"
   }
   tags = {
-    Name = "shipping-task-definition"
+    Name = "test-products-task-definition"
   }
 }
 
-resource "aws_ecs_task_definition" "payments_definition" {
-
-  family                   = "payments-task-definition"
+resource "aws_ecs_task_definition" "prod_products_definition" {
+  family                   = "prod-products-task-definition"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
+  cpu                      = "512"
+  memory                   = "1024"
   task_role_arn            = "arn:aws:iam::931481537897:role/LabRole"
   execution_role_arn       = "arn:aws:iam::931481537897:role/LabRole"
 
   container_definitions = jsonencode([{
-    name      = "payments-container"
-    image     = "931481537897.dkr.ecr.us-east-1.amazonaws.com/payments-service-ecr-repo:latest"
+    name      = "products-container"
+    image     = "931481537897.dkr.ecr.us-east-1.amazonaws.com/products-service-ecr-repo:latest"
     cpu       = 0
     essential = true
 
@@ -108,7 +107,7 @@ resource "aws_ecs_task_definition" "payments_definition" {
     logConfiguration = {
       logDriver = "awslogs"
       options = {
-        awslogs-group         = "/ecs/payments-task-definition"
+        awslogs-group         = "/ecs/prod-products-task-definition"
         awslogs-create-group  = "true"
         awslogs-region        = "us-east-1"
         awslogs-stream-prefix = "ecs"
@@ -120,9 +119,12 @@ resource "aws_ecs_task_definition" "payments_definition" {
     operating_system_family = "LINUX"
   }
   tags = {
-    Name = "payments-task-definition"
+    Name = "prod-products-task-definition"
   }
 }
+
+# TASK DEFINITION ORDERS #
+
 
 resource "aws_ecs_task_definition" "dev_orders_definition" {
   family                   = "dev-orders-task-definition"
@@ -154,16 +156,21 @@ resource "aws_ecs_task_definition" "dev_orders_definition" {
     logConfiguration = {
       logDriver = "awslogs"
       options = {
-        awslogs-group         = "/ecs/orders-task-definition"
+        awslogs-group         = "/ecs/dev-orders-task-definition"
         awslogs-create-group  = "true"
         awslogs-region        = "us-east-1"
         awslogs-stream-prefix = "ecs"
       }
     }
   }])
+  runtime_platform {
+    cpu_architecture        = "X86_64"
+    operating_system_family = "LINUX"
+  }
+  tags = {
+    Name = "dev-orders-task-definition"
+  }
 }
-
-
 
 resource "aws_ecs_task_definition" "test_orders_definition" {
   family                   = "test-orders-task-definition"
@@ -195,15 +202,21 @@ resource "aws_ecs_task_definition" "test_orders_definition" {
     logConfiguration = {
       logDriver = "awslogs"
       options = {
-        awslogs-group         = "/ecs/orders-task-definition"
+        awslogs-group         = "/ecs/test-orders-task-definition"
         awslogs-create-group  = "true"
         awslogs-region        = "us-east-1"
         awslogs-stream-prefix = "ecs"
       }
     }
   }])
+  runtime_platform {
+    cpu_architecture        = "X86_64"
+    operating_system_family = "LINUX"
+  }
+  tags = {
+    Name = "test-orders-task-definition"
+  }
 }
-
 
 resource "aws_ecs_task_definition" "prod_orders_definition" {
   family                   = "prod-orders-task-definition"
@@ -235,11 +248,270 @@ resource "aws_ecs_task_definition" "prod_orders_definition" {
     logConfiguration = {
       logDriver = "awslogs"
       options = {
-        awslogs-group         = "/ecs/orders-task-definition"
+        awslogs-group         = "/ecs/prod-orders-task-definition"
         awslogs-create-group  = "true"
         awslogs-region        = "us-east-1"
         awslogs-stream-prefix = "ecs"
       }
     }
   }])
+  runtime_platform {
+    cpu_architecture        = "X86_64"
+    operating_system_family = "LINUX"
+  }
+  tags = {
+    Name = "prod-orders-task-definition"
+  }
+}
+
+
+
+### SHIPPING TASK DEFINITION
+
+resource "aws_ecs_task_definition" "dev_shipping_definition" {
+  family                   = "dev-shipping-task-definition"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  task_role_arn            = "arn:aws:iam::931481537897:role/LabRole"
+  execution_role_arn       = "arn:aws:iam::931481537897:role/LabRole"
+
+  container_definitions = jsonencode([{
+    name      = "shipping-container"
+    image     = "931481537897.dkr.ecr.us-east-1.amazonaws.com/shipping-service-ecr-repo:latest"
+    cpu       = 0
+    essential = true
+
+    portMappings = [{
+      containerPort = 8080
+      hostPort      = 8080
+      protocol      = "tcp"
+      appProtocol   = "http"
+    }]
+
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = "/ecs/dev-shipping-task-definition"
+        awslogs-create-group  = "true"
+        awslogs-region        = "us-east-1"
+        awslogs-stream-prefix = "ecs"
+      }
+    }
+  }])
+  runtime_platform {
+    cpu_architecture        = "X86_64"
+    operating_system_family = "LINUX"
+  }
+  tags = {
+    Name = "dev-shipping-task-definition"
+  }
+}
+
+resource "aws_ecs_task_definition" "test_shipping_definition" {
+  family                   = "test-shipping-task-definition"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  task_role_arn            = "arn:aws:iam::931481537897:role/LabRole"
+  execution_role_arn       = "arn:aws:iam::931481537897:role/LabRole"
+
+  container_definitions = jsonencode([{
+    name      = "shipping-container"
+    image     = "931481537897.dkr.ecr.us-east-1.amazonaws.com/shipping-service-ecr-repo:latest"
+    cpu       = 0
+    essential = true
+
+    portMappings = [{
+      containerPort = 8080
+      hostPort      = 8080
+      protocol      = "tcp"
+      appProtocol   = "http"
+    }]
+
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = "/ecs/test-shipping-task-definition"
+        awslogs-create-group  = "true"
+        awslogs-region        = "us-east-1"
+        awslogs-stream-prefix = "ecs"
+      }
+    }
+  }])
+  runtime_platform {
+    cpu_architecture        = "X86_64"
+    operating_system_family = "LINUX"
+  }
+  tags = {
+    Name = "test-shipping-task-definition"
+  }
+}
+
+resource "aws_ecs_task_definition" "prod_shipping_definition" {
+  family                   = "prod-shipping-task-definition"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "1024"
+  task_role_arn            = "arn:aws:iam::931481537897:role/LabRole"
+  execution_role_arn       = "arn:aws:iam::931481537897:role/LabRole"
+
+  container_definitions = jsonencode([{
+    name      = "shipping-container"
+    image     = "931481537897.dkr.ecr.us-east-1.amazonaws.com/shipping-service-ecr-repo:latest"
+    cpu       = 0
+    essential = true
+
+    portMappings = [{
+      containerPort = 8080
+      hostPort      = 8080
+      protocol      = "tcp"
+      appProtocol   = "http"
+    }]
+
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = "/ecs/prod-shipping-task-definition"
+        awslogs-create-group  = "true"
+        awslogs-region        = "us-east-1"
+        awslogs-stream-prefix = "ecs"
+      }
+    }
+  }])
+  runtime_platform {
+    cpu_architecture        = "X86_64"
+    operating_system_family = "LINUX"
+  }
+  tags = {
+    Name = "prod-shipping-task-definition"
+  }
+}
+
+### PAYMENTS TASK DEFINITION
+
+resource "aws_ecs_task_definition" "dev_payments_definition" {
+  family                   = "dev-payments-task-definition"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  task_role_arn            = "arn:aws:iam::931481537897:role/LabRole"
+  execution_role_arn       = "arn:aws:iam::931481537897:role/LabRole"
+
+  container_definitions = jsonencode([{
+    name      = "payments-container"
+    image     = "931481537897.dkr.ecr.us-east-1.amazonaws.com/payments-service-ecr-repo:latest"
+    cpu       = 0
+    essential = true
+
+    portMappings = [{
+      containerPort = 8080
+      hostPort      = 8080
+      protocol      = "tcp"
+      appProtocol   = "http"
+    }]
+
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = "/ecs/dev-payments-task-definition"
+        awslogs-create-group  = "true"
+        awslogs-region        = "us-east-1"
+        awslogs-stream-prefix = "ecs"
+      }
+    }
+  }])
+  runtime_platform {
+    cpu_architecture        = "X86_64"
+    operating_system_family = "LINUX"
+  }
+  tags = {
+    Name = "dev-payments-task-definition"
+  }
+}
+
+resource "aws_ecs_task_definition" "test_payments_definition" {
+  family                   = "test-payments-task-definition"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  task_role_arn            = "arn:aws:iam::931481537897:role/LabRole"
+  execution_role_arn       = "arn:aws:iam::931481537897:role/LabRole"
+
+  container_definitions = jsonencode([{
+    name      = "payments-container"
+    image     = "931481537897.dkr.ecr.us-east-1.amazonaws.com/payments-service-ecr-repo:latest"
+    cpu       = 0
+    essential = true
+
+    portMappings = [{
+      containerPort = 8080
+      hostPort      = 8080
+      protocol      = "tcp"
+      appProtocol   = "http"
+    }]
+
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = "/ecs/test-payments-task-definition"
+        awslogs-create-group  = "true"
+        awslogs-region        = "us-east-1"
+        awslogs-stream-prefix = "ecs"
+      }
+    }
+  }])
+  runtime_platform {
+    cpu_architecture        = "X86_64"
+    operating_system_family = "LINUX"
+  }
+  tags = {
+    Name = "test-payments-task-definition"
+  }
+}
+
+resource "aws_ecs_task_definition" "prod_payments_definition" {
+  family                   = "prod-payments-task-definition"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "1024"
+  task_role_arn            = "arn:aws:iam::931481537897:role/LabRole"
+  execution_role_arn       = "arn:aws:iam::931481537897:role/LabRole"
+
+  container_definitions = jsonencode([{
+    name      = "payments-container"
+    image     = "931481537897.dkr.ecr.us-east-1.amazonaws.com/payments-service-ecr-repo:latest"
+    cpu       = 0
+    essential = true
+
+    portMappings = [{
+      containerPort = 8080
+      hostPort      = 8080
+      protocol      = "tcp"
+      appProtocol   = "http"
+    }]
+
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = "/ecs/prod-payments-task-definition"
+        awslogs-create-group  = "true"
+        awslogs-region        = "us-east-1"
+        awslogs-stream-prefix = "ecs"
+      }
+    }
+  }])
+  runtime_platform {
+    cpu_architecture        = "X86_64"
+    operating_system_family = "LINUX"
+  }
+  tags = {
+    Name = "prod-payments-task-definition"
+  }
 }
